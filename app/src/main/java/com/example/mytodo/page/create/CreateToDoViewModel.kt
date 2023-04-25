3e6b12712f692289bd120c25cc65779328da2236
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-04-25 16:21:00 +0900

    inject by koin instead of dagger hilt

--- app/src/main/java/com/example/mytodo/page/create/CreateToDoViewModel.kt
+++ app/src/main/java/com/example/mytodo/page/create/CreateToDoViewModel.kt
@@ -4,12 +4,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mytodo.repository.todo.ToDoRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@HiltViewModel
-class CreateToDoViewModel @Inject constructor(
+class CreateToDoViewModel(
     private val repo: ToDoRepository
 ): ViewModel() {
     val errorMessage = MutableLiveData<String>()
--- app/src/main/java/com/example/mytodo/page/detail/ToDoDetailFragment.kt
+++ app/src/main/java/com/example/mytodo/page/detail/ToDoDetailFragment.kt
@@ -3,7 +3,8 @@ package com.example.mytodo.page.detail
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.mytodo.R
+import org.koin.android.ext.android.inject
 
-class ToDoDetailFragment:Fragment(R.layout.todo_detail_fragment) {
-    private val vm: ToDoDetailViewModel by viewModels()
+class ToDoDetailFragment : Fragment(R.layout.todo_detail_fragment) {
+    private val vm: ToDoDetailViewModel by inject()
 }
