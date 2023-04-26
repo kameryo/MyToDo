77a910b8441910aaf8ee63b2c8820102c14d6209
Ryouh <ryouhei629@icloud.com>
2023-04-26 10:50:17 +0900

    first commit

--- app/src/main/java/com/example/mytodo/MainViewModel.kt
+++ app/src/main/java/com/example/mytodo/MainViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.mytodo
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.mytodo.repository.todo.ToDoRepository
 
-class MainViewModel: ViewModel() {
+class MainViewModel(
+    private val repo: ToDoRepository
+): ViewModel() {
+
+    val todoList = repo.getAll().asLiveData()
 }
--- app/src/main/java/com/example/mytodo/repository/todo/ToDoRepository.kt
+++ app/src/main/java/com/example/mytodo/repository/todo/ToDoRepository.kt
@@ -1,6 +1,10 @@
 package com.example.mytodo.repository.todo
 
+import com.example.mytodo.model.todo.ToDo
+import kotlinx.coroutines.flow.Flow
+
 interface ToDoRepository {
+    fun getAll(): Flow<List<ToDo>>
     suspend fun create(title: String, detail: String)
 
 }
