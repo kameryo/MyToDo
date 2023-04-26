ff9903e52c7e08e848cfcfa6775b7405748f1701
Ryouh <ryouhei629@icloud.com>
2023-04-26 22:36:32 +0900

    リスト表示機能の実装2

--- app/src/main/java/com/example/mytodo/repository/todo/ToDoRepositoryImpl.kt
+++ app/src/main/java/com/example/mytodo/repository/todo/ToDoRepositoryImpl.kt
@@ -5,17 +5,20 @@ import com.example.mytodo.model.todo.ToDoDAO
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class ToDoRepositoryImpl(private val dao: ToDoDAO): ToDoRepository{
-
-    override fun getAll(): Flow<List<ToDo>> {
-        return dao.getAll()
-    }
+class ToDoRepositoryImpl @Inject constructor(
+    private val dao: ToDoDAO
+): ToDoRepository{
     override suspend fun create(title: String, detail: String) {
         val now = System.currentTimeMillis()
         val todo = ToDo(title = title, detail = detail, created = now, modified = now)
         withContext(Dispatchers.IO) {
             dao.create(todo)
         }
+    }
+
+    override fun getAll(): Flow<List<ToDo>> {
+        return dao.getAll()
     }
 }
