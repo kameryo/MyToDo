0a721173c5d90046d7d4b8872520f253a987577a
Ryouh <ryouhei629@icloud.com>
2023-04-27 12:01:08 +0900

    ToDoの詳細表示

--- app/src/main/java/com/example/mytodo/ToDoAdapter.kt
+++ app/src/main/java/com/example/mytodo/ToDoAdapter.kt
@@ -9,12 +9,22 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mytodo.databinding.TodoItemBinding
 import com.example.mytodo.model.todo.ToDo
 
-class ToDoAdapter: ListAdapter<ToDo, ToDoAdapter.ViewHolder>(callbacks) {
+class ToDoAdapter(
+    private val listener: (ToDo) -> Unit
+): ListAdapter<ToDo, ToDoAdapter.ViewHolder>(callbacks) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = TodoItemBinding.inflate(inflater, parent, false)
-        return ViewHolder(binding)
+
+        val viewHolder = ViewHolder(binding)
+
+        binding.root.setOnClickListener{
+            val position = viewHolder.bindingAdapterPosition
+            val todo = getItem(position)
+            listener(todo)
+        }
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
--- app/src/main/java/com/example/mytodo/model/todo/ToDo.kt
+++ app/src/main/java/com/example/mytodo/model/todo/ToDo.kt
@@ -1,9 +1,13 @@
 package com.example.mytodo.model.todo
 
+
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 @Entity
+@Parcelize
 data class ToDo(
     @PrimaryKey(autoGenerate = true)
     val _id: Int = 0,
@@ -11,4 +15,4 @@ data class ToDo(
     val detail: String,
     val created: Long,
     val modified: Long
-)
+): Parcelable
